ccdebc1183be55e14f8749a248611218ac76daa3
kerollos <kerolloz@yahoo.com>
2018-07-03 21:16:01 +0200

    removed tester.py, add it's code to parser instead

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import os
 import urllib.error
+import os
 from urllib.request import urlopen
 from tkinter import *
 from tkinter import ttk
@@ -31,11 +31,66 @@ int main (){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     // come on, go SOLVE it..!!!
-    // save your code, then use tester.exe to check the test cases
+    // save your code, then use tester.py to check the test cases
 
 
     return 0;
 }
+'''
+
+tester_code = r'''#!/usr/bin/python3
+import os
+import sys
+from termcolor import cprint
+import subprocess
+
+
+test_cases = int(open('test_cases.txt', 'r').read())
+
+input_file_names = ['in' + str(e) + '.txt' for e in range(test_cases)]
+output_file_names = ['out' + str(e) + '.txt' for e in range(test_cases)]
+
+os.system('clear')
+
+cprint('Compiling...', 'white', attrs=['dark'])
+
+if os.system('g++ main.cpp') == 0:  # returned 0 = successful
+    os.system('clear')
+    cprint('Compiled successfully...!\n', 'green', attrs=['bold'])
+else:
+    cprint('Compilation ERROR\n\n' + "Please, Check Your code", 'red',  attrs=['bold'])
+    cprint('\nPress ENTER to exit...', end='', color='white')
+    input()
+    sys.exit()
+    # stop and close the program
+
+ac = True
+
+for e in range(test_cases):
+    os.system('./a.out < ' + input_file_names[e] + ' > my_' + output_file_names[e])
+
+    cprint('Test Case {}:'.format(str(e + 1)), 'yellow')
+
+    diff_command = subprocess.getstatusoutput('diff -s -q -Z ' + output_file_names[e] + ' my_' + output_file_names[e])
+    # diff_command returns a tuple that contains 2 items(the exit status, command output)
+    # if zero(no error), successful
+    exit_status = diff_command[0]
+
+    if not exit_status:
+        cprint(' Passed', 'green')
+    else:
+        ac = False
+        cprint(' Wrong Answer', 'red')
+
+print()
+
+if ac:
+    cprint("ACCEPTED", 'green', attrs=['reverse', 'bold'])
+else:
+    cprint('Try Harder!', 'white', 'on_blue')
+
+cprint('\nPress ANY KEY to exit...', end='', color='white')
+input()
 '''
 
 
@@ -153,9 +208,11 @@ class Gui:
         directory_name = directory_name.replace('/', '')  # remove slash '/' form the directory name to avoid confusion
 
         os.system('mkdir ' + directory_name)  # create a new folder
-        os.system('cp test.py ' + directory_name + '/')  # copy tester.exe into the problem folder
+        # os.system('cp test.py ' + directory_name + '/')  # copy tester.exe into the problem folder
         os.chdir(directory_name)  # go to the problem folder
 
+        with open('tester.py', 'w') as tester:
+            tester.write(tester_code)
         with open('main.cpp', 'w') as code:
             code.write(c_code)
 
