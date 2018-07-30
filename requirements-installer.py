272c18c763cf0013d50ab20ab29813f9d0d2be62
kerollos <kerolloz@yahoo.com>
2018-07-30 09:22:00 +0200

    Version 1.3 :100:

--- requirements-installer.py
+++ requirements-installer.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 import os
 import subprocess
+from termcolor import cprint
 
 if __name__ == '__main__':
     username = subprocess.check_output('whoami').decode()  # save the returned output
 
     os.system('clear')
 
-    print('Hey, ' + username + 'Welcome to Requirements-Installer...\n')
-    print('Programs will be installed from the following file')
+    cprint('Hey, ' + username + 'Welcome to Requirements-Installer...\n', 'yellow')
+    cprint('Programs will be installed from the following file', 'green')
 
     dir_path = str(os.path.dirname(os.path.realpath(__file__)) + '/requirements.txt')
     # I'm using this way so you can run the file from any place using the terminal
-    print(dir_path)
+    cprint(dir_path, 'magenta')
 
     installation_list = open(dir_path, 'r')
-    print('\nThe following programs will be installed:\n') 
+    cprint('\nThe following programs will be installed:\n', 'cyan') 
     for program in installation_list:
-        print('-' + program.strip())        
+        cprint('-' + program.strip(), 'blue')        
     print()
     installation_list.seek(0)
         
